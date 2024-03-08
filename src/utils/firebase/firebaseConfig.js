1071154d2805ed57ff66710a67eab6f0b8e83c81
unknown <xvelasco1@ucol.mx>
2024-03-08 13:45:59 -0600

    WIP

--- src/utils/firebase/firebaseConfig.js
+++ src/utils/firebase/firebaseConfig.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN_FIREBASE,
-  projectId: process.env.REACT_APP_PROJECT_ID_FIREBASE,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET_FIREBASE,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_FIREBASE,
-  appId: process.env.REACT_APP_ID_FIREBASE,
+  apiKey: import.meta.env.VITE_APP_API_KEY_FIREBASE,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN_FIREBASE,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID_FIREBASE,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID_FIREBASE,
+  appId: import.meta.env.VITE_APP_ID_FIREBASE,
 };
 
 const app = initializeApp(firebaseConfig);
